ba40cfc1b8a09b99de869fe851610e4122a2049e
MohannadBT <xsmile3x@gmail.com>
2025-04-27 15:16:15 +0300

    End of section 33

---
+++ app/_components/Counter.js
@@ -0,0 +1,15 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Counter({ users }) {
+  const [count, setCount] = useState(0);
+  console.log(users);
+
+  return (
+    <div>
+      <p>There is {users.length} users</p>
+      <button onClick={() => setCount((c) => c + 1)}>Counter: {count}</button>
+    </div>
+  );
+}
